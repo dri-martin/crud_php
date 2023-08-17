f12a13dc0fae2975d4fc326c57e13c6ee8df1e6e
dri-marti <andrymartins91@gmail.com>
2023-08-16 22:04:02 -0300

    segunda parte do crud

--- classes/Crud.php
+++ classes/Crud.php
@@ -1,50 +1,80 @@
 <?php
-include_once('conexao/conexao.php');
+include_once('Conexao/conexao.php');
 
 $db = new Database();
 
 class Crud{
     private $conn;
     private $table_name = "carros";
-    
+
     public function __construct($db){
         $this->conn = $db;
     }
 
-//função parar Criar meu registros
-public function create($postValues){
-    $modelo = $postValues['modelo'];
-    $marca = $postValues['marca'];
-    $placa = $postValues['placa'];
-    $cor = $postValues['cor'];
-    $ano = $postValues['ano'];
+    public function create($postValues){
+        $modelo = $postValues['modelo'];
+        $marca = $postValues['marca'];
+        $placa = $postValues['placa'];
+        $cor = $postValues['cor'];
+        $ano = $postValues['ano'];
 
+        $query = "INSERT INTO ". $this->table_name . " (modelo, marca, placa, cor, ano) VALUES (?,?,?,?,?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1,$modelo);
+        $stmt->bindParam(2,$marca);
+        $stmt->bindParam(3,$placa);
+        $stmt->bindParam(4,$cor);
+        $stmt->bindParam(5,$ano);
 
-    $query = "INSERT INTO ". $this->table_name . " (modelo, marca, placa, cor, ano) VALUES (?,?,?,?,?)";
-    $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(1,$modelo);
-    $stmt->bindParam(2,$marca);
-    $stmt->bindParam(3,$placa);
-    $stmt->bindParam(4,$cor);
-    $stmt->bindParam(5,$ano);
-
-    $rows = $this->read();
-    if($stmt->execute()){
-        print "<script>alert('Cadastro Ok!')</script>";
-        print "<script> localtion.href='?action=read'; </script>";
-        return true;
-    }else{
-        return false;
+        $rows = $this->read();
+        if($stmt->execute()){
+            print "<script>alert('Cadastro Ok!')</script>";
+            print "<script> location.href='?action=read'; </script>";
+            return true;
+        }else{
+            return false;
+        }
     }
-  }
 
-  
-  public function read(){
-    $query = "SELECT * FROM ". $this->table_name;
-    $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-    return $stmt;
-  }
+    public function read(){
+        $query = "SELECT * FROM ". $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+    public function update($postValues){
+        $id = $postValues['id'];
+        $modelo = $postValues['modelo'];
+        $marca = $postValues['marca'];
+        $placa = $postValues['placa'];
+        $cor = $postValues['cor'];
+        $ano = $postValues['ano'];
 
+        if(empty($id) || empty($modelo) || empty($marca) || empty($placa) || empty($cor) || empty($ano)){
+            return false; 
+        }
+        $query = "UPDATE". $this->table_name . " SET modelo = ?, marca = ?, placa = ?, cor = ?, ano = ? WERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->blindParam(1,$modelo);
+        $stmt->blindParam(2,$marca);
+        $stmt->blindParam(3,$placa);
+        $stmt->blindParam(4,$cor);
+        $stmt->blindParam(5,$ano);
+        $stmt->blindParam(6,$id);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public function readOne($id){
+        $query = "SELECT * FROM ". $this->table_name . " WHERE id = ?";
+        $stmt =$this->conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -107,6 +107,36 @@ default:
 
 </head>
 <body>
+        <?php
+
+        if(isset($GET['action']) && $_GET['action'] == 'update' && isset($_GET['id'])){
+            $id = $_GET['id'];
+            $result =$crud->readOne($id);
+
+            if($result){
+                echo "Registro não encontrado.";
+                exit();
+            }
+            $modelo  = $result['modelo'];
+            $marca  = $result['marca'];
+            $placa  = $result['placa'];
+            $cor  = $result['cor'];
+            $ano= $result['ano'];
+        
+
+        ?>
+
+
+
+
+
+
+
+
+
+
+
+
     <form action="?action=create" method="POST">
         <label for="">Modelo</label>
         <input type="text" name="modelo">
@@ -135,8 +165,29 @@ default:
             <td>Cor</td>
             <td>Ano</td>
         </tr>
-
-
+    
+        <?php
+  if($rows->rowCount() == 0){
+    echo "<tr>";
+    echo "<td colspan='7'>Nenhum dado encontrado</td>";
+    echo "</tr>";
+  } else {
+    while($row = $rows->fetch(PDO::FETCH_ASSOC)){
+      echo "<tr>";
+      echo "<td>" . $row['id'] . "</td>";
+      echo "<td>" . $row['modelo'] . "</td>";
+      echo "<td>" . $row['marca'] . "</td>";
+      echo "<td>" . $row['placa'] . "</td>";
+      echo "<td>" . $row['cor'] . "</td>";
+      echo "<td>" . $row['ano'] . "</td>";
+      echo "<td>";
+      echo "<a href='?action=update&id=" . $row['id'] . "'>Update</a>";
+      echo "<a href='?action=delete&id=" . $row['id'] . "' onclick='return confirm(\"Tem certeza que quer apagar esse registro?\")' class='delete'>Delete</a>";
+      echo "</td>";
+      echo "</tr>";
+    }
+  }
+?>
 
     </table>
 </body>
